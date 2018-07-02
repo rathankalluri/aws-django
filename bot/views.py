2623d47d50ec21d06738e734d09cee979acd8b98
Ratna Kumar K V V <ratan.kalluri@gmail.com>
2018-07-02 16:30:39 +0530

    Update views.py

--- bot/views.py
+++ bot/views.py
@@ -220,19 +220,19 @@ def chat(request):
 		
 		#Check invidiual instances details
 		if instance_id and (instance_state not in STATES) and (meta not in META):
-			return JsonResponse(check_status(instance_id=instance_id))
+			return JsonResponse(dict(check_status(instance_id=instance_id)))
 			
 		if not instance_id and (instance_state in STATES):
 			if instance_state == 'running':
-				return JsonResponse(check_status(filter='running'))
+				return JsonResponse(dict(check_status(filter='running')))
 			if instance_state == 'stopping':
-				return JsonResponse(check_status(filter='stopping'))
+				return JsonResponse(dict(check_status(filter='stopping')))
 			if instance_state == 'stopped':
-				return JsonResponse(check_status(filter='stopped'))
+				return JsonResponse(dict(check_status(filter='stopped')))
 			if instance_state == 'shutting-down': 
-				return JsonResponse(check_status(filter='shutting-down'))
+				return JsonResponse(dict(check_status(filter='shutting-down')))
 			if instance_state == 'terminated':
-				return JsonResponse(check_status(filter='terminated'))
+				return JsonResponse(dict(check_status(filter='terminated')))
 		else:
 			return JsonResponse({"fulfillmentText": "This is a text response","payload":{"slack": {"text": "Use any of these keywords (running|stopping|stopped|shutting-down|terminated)"}}}) #Error Message
 	return render(request, 'bot/data.html', context={'nooutput':"You landed on a wrong page please go back to Home page"},)
