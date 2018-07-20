a2edea6bc46ce528d4b716aead135d1c68da92f0
Ratna Kumar K V V <ratan.kalluri@gmail.com>
2018-07-20 17:41:30 +0530

    Update settings.py

--- awsbot/settings.py
+++ awsbot/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+#Site URL
+SITE_URL = 'https://aws-bot.herokuapp.com/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
